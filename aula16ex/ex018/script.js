55b412f50fc5f441520dcbb08bf1929e9e5d0f64
Thiago <thiagovini200@gmail.com>
2020-05-29 11:02:35 -0300

    Exercícios JavaScript (Parte 7 e 8) - Curso JavaScript

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -2,8 +2,7 @@ var b = []
 let resf = document.getElementById("resf")
 
 
-function calcular() {
-    console.log("##INICIO DO CALCULAR##")
+function adicionar() {
     let n = document.getElementById("txtnum")
     let res = document.getElementById("res")
     let n_num = Number(n.value)
@@ -14,32 +13,40 @@ function calcular() {
         let item = document.createElement('option')
         item.text += `Valor ${n_num} adicionado.`
         res.appendChild(item)
-        console.log("B calcular antes do push " + b)
+        
         b.push(n_num)
-        console.log("B calcular depois do push -->" + b)
     }
-    console.log("##FIM DO CALCULAR##")
+    n.value = ''
+    n.focus()
     resf.innerHTML = ''
 }
 
 function finalizar() {
-    console.log("##INICIO DO FINALIZAR##")
     var ar = []
     ar = b;
     ar.sort( function(x, y){
         return x - y;
     });
+
     /*
     O "Sort()" classifica os valores como string() e os ordena em ordem ascendente. Ou seja, se seu array for de valores numéricos ele não terá a ordenação que você deseja. Pois o valor será interpretado como uma string e aí sim ordenado, então usei o metodo acima.
     */
+   if (b.length ==0){
+       window.alert('Adicione valores antes de finalizar')
+   } else {
+        resf.innerHTML += `<p>Ao todo, temos ${b.length} números cadastrados.</p>`
 
-    console.log("depois de reorganizar" + ar)
-    
+        resf.innerHTML += `<p>O maior valor informado foi ${ar[ar.length - 1]}.</p>`
 
-    
+        resf.innerHTML += `<p>O menor valor informado foi ${ar[0]}.</p>`
+        var soma = 0 
 
-    resf.innerHTML += `<p>Ao todo, temos ${b.length} números cadastrados.</p>`
+        for(let c = 0; c < b.length; c++) {
+            soma = soma + b[c]
+        }
 
-    resf.innerHTML += `<p>O maior valor informado foi ${ar[ar.length - 1]}.</p>`
-    
+        resf.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        
+        resf.innerHTML += `<p>A média dos valores digitados é ${soma / b.length}.</p>`
+    }    
 }
